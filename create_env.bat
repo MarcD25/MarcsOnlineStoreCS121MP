0a4de346f716190b2fd5c539a7eddb0d14df45a6
UPMarcD <mgdoria@up.edu.ph>
2025-04-12 09:15:31 +0800

    chore: remove create_env.bat from git tracking and update .gitignore

--- create_env.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-:: Create .env file for the backend
-cd store-backend
-(echo DATABASE_URL=postgresql://<username>:<password>@localhost:5432/ecommerce) > .env
-
-echo .env file created successfully in store-backend directory.
